3ba5b9f1a803f747a775e6f2de8841fb8781e85c
Aiman <ahardao1001@g.educaand.es>
2024-05-13 14:44:42 +0200

    c4

--- src/main/java/daw/Principal.java
+++ src/main/java/daw/Principal.java
@@ -39,7 +39,7 @@ public class Principal extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "Nombre", "Fecha Nacimiento", "Grupo Sanguíneo","RH","NºDonaciones"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
