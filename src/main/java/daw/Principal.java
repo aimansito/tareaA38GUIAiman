282302e9bccac4e8f71ad297ecf338254837f861
Aiman <ahardao1001@g.educaand.es>
2024-05-13 09:58:09 +0200

    c2

--- src/main/java/daw/Principal.java
+++ src/main/java/daw/Principal.java
@@ -26,17 +26,39 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(72, 72, 72)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 441, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(414, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(66, 66, 66)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 313, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(145, Short.MAX_VALUE))
         );
 
         pack();
@@ -78,5 +100,7 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
--- src/main/java/modelos/ModeloPacientes.java
+++ src/main/java/modelos/ModeloPacientes.java
@@ -4,10 +4,29 @@
  */
 package modelos;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author aiman
  */
-public class ModeloPacientes {
+public class ModeloPacientes extends DefaultTableModel{
+    public ModeloPacientes(){
+        // Se asignan los nombres de las columnas de la tabla
+        // en función de los atributos que tiene la persona
+        String[] columnNames = {"ID", "NOMBRE", "APELLIDO", "CIUDAD"}; 
+        
+        // Se le indica al modelo el nombre de las columnas y cantidad
+        this.setColumnIdentifiers(columnNames); 
+    }
     
+    // Este método sobrescrito sirve para indicar qué celdas
+    // del JTable son editables
+    public boolean isCellEditable (int row, int column)
+    {
+        // Aquí devolvemos true o false según queramos que una celda
+        // identificada por fila,columna (row,column), sea o no editable
+        // En nuestro caso ninguna celda se edita
+        return false;
+    }
 }
